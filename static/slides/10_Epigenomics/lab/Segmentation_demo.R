b7a9d5107e382562f8ea0835c69d61570a3b79ac
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2021-04-07 11:05:34 -0400

    Make HMM demo

--- static/slides/10_Epigenomics/lab/Segmentation_demo.R
+++ static/slides/10_Epigenomics/lab/Segmentation_demo.R
@@ -5,57 +5,29 @@ library(nullranges)
 library(EnsDb.Hsapiens.v86)
 library(GenomeInfoDb)
 
+# Get list of genes
 edb <- EnsDb.Hsapiens.v86
-g <- genes(edb, filter=AnnotationFilterList(GeneIdFilter("ENSG", "startsWith")))
-x <- keepSeqlevels(g,as.character(seq_len(12)),pruning.mode="coarse")
+g <- genes(edb, filter = AnnotationFilterList(GeneIdFilter("ENSG", "startsWith")))
+x <- keepSeqlevels(g, as.character(seq_len(12)), pruning.mode = "coarse")
 
-Ls=1e6
-n=3
-seg<-segment_density(g1,n=3,Ls=Ls,type = "HMM",boxplot = TRUE)
-
-query <- tileGenome(seqlengths(x)[seqnames(x)@values], tilewidth = Ls, cut.last.tile.in.chrom = TRUE)
+# Tile the genome into 1M basepair windows
+query <- tileGenome(seqlengths(x)[seqnames(x)@values], tilewidth = 1e6, cut.last.tile.in.chrom = TRUE)
+# Calculate gene density per window
 counts <- countOverlaps(query, x)
+# Plot the results, with a little noise
 eps <- rnorm(length(counts), 0, .2)
 plot(sqrt(counts) + eps)
 
-# CBS
-library(DNAcopy)
-cna <- CNA(matrix(sqrt(counts) + eps, ncol = 1),
-           chrom = as.character(seqnames(query)), # wont work for X,Y,MT
-           maploc = start(query),
-           data.type = "logratio",
-           presorted = TRUE
-)
-scna <- segment(cna,
-                undo.splits = "sdundo",
-                undo.SD = 1.5,
-                verbose = 1
-)
-seq <- with(scna$output, rep(seg.mean, num.mark))
-# plot(scna)
-# plot(seq)
-q <- quantile(seq, .95)
-seq2 <- pmin(seq, q)
-# plot(seq2)
-km <- kmeans(seq2, n)
-query$states <- km$cluster
-plot(sqrt(counts) + eps, col = km$cluster)
-
 # HMM
 library(RcppHMM)
-hmm <- initPHMM(n)
+hmm <- initPHMM(n = 3) # Three states
 hmm <- learnEM(hmm,
-               counts,
-               iter = 400,
-               delta = 1e-5,
-               print = TRUE
+  counts,
+  iter = 400,
+  delta = 1e-5,
+  print = TRUE
 )
 hmm
+# Find states with the Viterbi algorithm
 v <- as.integer(factor(viterbi(hmm, counts), levels = hmm$StateNames))
 plot(sqrt(counts) + eps, col = v)
-
-
-
-
-
-
