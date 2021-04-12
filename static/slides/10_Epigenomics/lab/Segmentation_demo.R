7131bf2323223143981f06bd18e0cd05742e13a4
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2021-04-12 08:24:55 -0400

    Segmentation example polishing

--- static/slides/10_Epigenomics/lab/Segmentation_demo.R
+++ static/slides/10_Epigenomics/lab/Segmentation_demo.R
@@ -1,33 +1,55 @@
 # Example taken from the nullranges package
 # https://github.com/nullranges/nullranges
 # BiocManager::install("nullranges/nullranges")
-library(nullranges)
+library(RcppHMM) # For Hidden Markov Models
+library(DNAcopy) # for Circular Binary Segmentation
 library(EnsDb.Hsapiens.v86)
 library(GenomeInfoDb)
 
 # Get list of genes
 edb <- EnsDb.Hsapiens.v86
 g <- genes(edb, filter = AnnotationFilterList(GeneIdFilter("ENSG", "startsWith")))
-x <- keepSeqlevels(g, as.character(seq_len(12)), pruning.mode = "coarse")
+x <- keepSeqlevels(g, c("2"), pruning.mode = "coarse") # Chr2 only as example
 
-# Tile the genome into 1M basepair windows
-query <- tileGenome(seqlengths(x)[seqnames(x)@values], tilewidth = 1e6, cut.last.tile.in.chrom = TRUE)
+# Tile the genome into windows
+query <- tileGenome(seqlengths(x)[seqnames(x)@values], tilewidth = 5e5, cut.last.tile.in.chrom = TRUE)
 # Calculate gene density per window
 counts <- countOverlaps(query, x)
-# Plot the results, with a little noise
-eps <- rnorm(length(counts), 0, .2)
-plot(sqrt(counts) + eps)
+# Plot the results
+plot(sqrt(counts))
 
 # HMM
-library(RcppHMM)
 hmm <- initPHMM(n = 3) # Three states
 hmm <- learnEM(hmm,
-  counts,
-  iter = 400,
-  delta = 1e-5,
-  print = TRUE
+               counts,
+               iter = 400,
+               delta = 1e-5,
+               print = TRUE
 )
-hmm
+# hmm
 # Find states with the Viterbi algorithm
 v <- as.integer(factor(viterbi(hmm, counts), levels = hmm$StateNames))
-plot(sqrt(counts) + eps, col = v)
+plot(sqrt(counts), col = v)
+
+# CBS
+cna <- CNA(matrix(sqrt(counts), ncol = 1),
+           chrom = as.character(seqnames(query)),
+           maploc = start(query),
+           data.type = "logratio",
+           presorted = TRUE
+)
+scna <- segment(cna,
+                undo.splits = "sdundo",
+                undo.SD = 1.5,
+                verbose = 1
+)
+seq <- with(scna$output, rep(seg.mean, num.mark))
+# plot(scna)
+# plot(seq)
+q <- quantile(seq, .95)
+seq2 <- pmin(seq, q)
+# plot(seq2)
+km <- kmeans(seq2, centers = 3)
+query$states <- km$cluster
+plot(sqrt(counts), col = km$cluster)
+
